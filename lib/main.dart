03595c65c38fa56e1b3c5d5393e31ebc6522c929
IQBAL HASAN <iqbalhasan.dev@gmail.com>
2021-10-27 11:34:04 +0600

    Dymic List

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(ListApp());
 
-class MyApp extends StatelessWidget {
-  // const MyApp({Key? key}) : super(key: key);
-
+class ListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("column Row"),
+          title: Text("List Exercise App"),
         ),
-        body: MyListView(),
+        body: ListViewContent(),
       ),
     );
   }
 }
 
-class MyListView extends StatelessWidget {
+class ListViewContent extends StatelessWidget {
+  final Zilla = [
+    "Rajshahi",
+    "Comilla",
+    "Sylhet",
+    "Pabna",
+    "Dhaka",
+    "Chottogram",
+    "Bangura"
+  ];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        ListTile(
-          title: Text('List 1'),
-        ),
-        ListTile(
-          title: Text('List 1'),
-        ),
-        ListTile(
-          title: Text('List 1'),
-        ),
-        ListTile(
-          title: Text('List 1'),
-        ),
-      ],
+    return ListView.builder(
+      itemCount: Zilla.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Container(
+            child: Text(Zilla[index]),
+          ),
+        );
+      },
     );
   }
 }
